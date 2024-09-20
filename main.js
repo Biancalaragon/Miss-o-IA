caa3030b003eadfe62a67d3657a8f4a16f57b36c
Biancalaragon <125667139+Biancalaragon@users.noreply.github.com>
2024-09-20 11:24:06 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,35 +1,94 @@
+
 const caixaPrincipal =  document.querySelector(".caixa-principal");
-const caixaPergunta =  document.querySelector(".caixa-pergunta");
-const caixaAlternativas =  document.querySelector(".caixa-alternativas");
-const caixaResultado =  document.querySelector(".caixa-resultado");
-const textoResultado =  document.querySelector(".texto-resultado");
-const perguntas = [
+ const caixaPergunta =  document.querySelector(".caixa-pergunta");
+ const caixaAlternativas =  document.querySelector(".caixa-alternativas");
+ const caixaResultado =  document.querySelector(".caixa-resultado");
+ const textoResultado =  document.querySelector(".texto-resultado");
+
+ const perguntas = [
     {
         enunciado: "Pergunta 1",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            {
+                texto: "Alternativa 1",
+                afirmacao: "afirmação"
+            },
+            {
+                 
+            texto: "Alternativa 2",
+            afirmacao: "afirmação"
+             }
         ]
     },
     {
         enunciado: "Pergunta 2",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            {
+                texto: "Alternativa 1",
+                afirmacao: "afirmação"
+            },
+            {
+                 
+            texto: "Alternativa 2",
+            afirmacao: "afirmação"
+            }
         ]
     },
     {
         enunciado: "Pergunta 3",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            {
+                texto: "Alternativa 1",
+                afirmacao: "afirmação"
+            },
+            {
+                 
+            texto: "Alternativa 2",
+            afirmacao: "afirmação"
+            }
         ]
     },
     {
         enunciado: "Pergunta 4",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            {
+                texto: "Alternativa 1",
+                afirmacao: "afirmação"
+            },
+            {
+                 
+            texto: "Alternativa 2",
+            afirmacao: "afirmação"
+            }
         ]
     },
-]
+ ];
+ 
+ 
+ let atual = 0 ;
+ let perguntaAtual;
+ 
+ 
+ function mostraPergunta() {
+ perguntaAtual = perguntas [atual];
+ caixaPerguntas.textContent = perguntaAtual.enunciado;
+ mostrarAlternativas();
+ }
+ 
+ function mostraAlternativas() {
+ for (const alternativa of perguntaAtual.alternativas) {
+ const botaoAlternativas = document. createElement("button");
+ botaoAlternativas.textContent = alternativa.texto;
+ botaoAlternativas.addEventListener ("click", function (){
+    atual++;
+    mostraPergunta();
+ })
+ caixaAlternativas.appendChild(botaoAlternativas);
+ }
+ }
+ 
+ 
+ mostraPergunta ();
+ 
+ 
+ 
